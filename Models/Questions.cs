ee425dee743a707c34899dabcf8280ecb09af3a8
r-sumair-12 <sumairrana888@gmail.com>
2026-07-12 07:36:37 +0500

    Add files via upload

---
+++ Models/Questions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.Models
+{
+    internal class Questions
+    {
+        public string questionstmt {  get; set; }
+        public int correctAnswerIndex { get; set; }
+        public string[] options { get; set; } = new string[4];
+        public bool isDone { get; set; }
+    }
+}
---
+++ Models/Quiz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.Models
+{
+    internal class Quiz
+    {
+        public List<Questions> Questions { get; set; }
+        public int score { get; set; }
+        public int correctAnswers { get; set;}
+        public int wrongAnswers { get; set;}
+        public int currentQuestion { get; set;}
+
+        public Quiz () {
+            Questions = new List<Questions>();
+            currentQuestion = 0;
+        }
+
+    }
+}
